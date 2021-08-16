9706e66eb3b6e5a9d278ad13189ec0db238fa0d4
Jared Verdi <jared@jaredverdi.com>
2021-08-15 23:49:20 -0400

    favicon, site titles, minor styling

--- js/packages/web/src/pages/_document.tsx
+++ js/packages/web/src/pages/_document.tsx
@@ -30,17 +30,11 @@ export default class MetaplexDocument extends Document {
           <link
             rel="icon"
             type="image/png"
-            sizes="96x96"
-            href="/favicon-96x96.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
             sizes="16x16"
             href="/favicon-16x16.png"
           />
           <meta name="theme-color" content="#000000" />
-          <meta name="description" content="Metaplex NFT Marketplace" />
+          <meta name="description" content="The Exiled Apes NFT Marketplace" />
           <link rel="manifest" href="/manifest.json" />
           <link
             rel="stylesheet"
